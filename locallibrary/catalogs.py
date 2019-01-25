6a67b1febfa039a9408f0f608ace69ba8d26ac05
Carrington R Tubman <ctubman@umich.edu>
2019-01-25 03:50:01 +0000

    Models tutorial complete

--- locallibrary/catalogs.py
+++ locallibrary/catalogs.py
@@ -5,10 +5,12 @@ from django.db import models
 class Genre(models.Model):
     """Model representing a book genre."""
     name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
-    
+
     def __str__(self):
         """String for representing the Model object."""
         return self.name
+
+
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
 
@@ -28,26 +30,24 @@ class Book(models.Model):
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    
+
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-    
+
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-    
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
-    
+
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
 
 import uuid # Required for unique book instances
-from datetime import date
-
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -96,4 +96,6 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
+
 
--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uqxqav46+5f#$bqdr+&g8g8pj^on4n^lp2=*vki7c1ls6%ihoh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig'
 ]
 
 MIDDLEWARE = [
