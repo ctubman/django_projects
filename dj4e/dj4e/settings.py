f47fb2d6e1344f914787e98dce6271a9268d44ad
Carrington R Tubman <ctubman@umich.edu>
2019-03-14 02:08:14 +0000

    364 final project

--- dj4e/dj4e/settings.py
+++ dj4e/dj4e/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'autos.apps.AutosConfig',
     'cats.apps.CatsConfig',
+    'wizards.apps.WizardsConfig',
 ]
 
 MIDDLEWARE = [
---
+++ dj4e/wizards/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from wizards.models import Wizard, House
+
+admin.site.register(Wizard)
+admin.site.register(House)
+# Register your models here.
+
