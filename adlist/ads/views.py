bae0542f9dd8a61481e6c57cbc74cf7f40774650
Carrington R Tubman <ctubman@umich.edu>
2019-04-02 22:00:59 +0000

    partial progress for adlist2

--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
 from ads.models import Ad
 from django.views import View
 from django.views import generic
-from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from ads.util import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from django.http import HttpResponse
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from ads.forms import CreateForm
+
 
 class AdListView(OwnerListView):
     model = Ad
@@ -29,3 +31,42 @@ class AdUpdateView(OwnerUpdateView):
 class AdDeleteView(OwnerDeleteView):
     model = Ad
     template_name = "ad_delete.html"
+
+
+class AdFormView(LoginRequiredMixin, View):
+    template = 'ad_form.html'
+    success_url = reverse_lazy('pics')
+    def get(self, request, pk=None) :
+        if not pk :
+            form = CreateForm()
+        else:
+            pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+            form = CreateForm(instance=pic)
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        if not pk:
+            form = CreateForm(request.POST, request.FILES or None)
+        else:
+            pic = get_object_or_404(Pic, id=pk, owner=self.request.user)
+            form = CreateForm(request.POST, request.FILES or None, instance=pic)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        # Adjust the model owner before saving
+        pic = form.save(commit=False)
+        pic.owner = self.request.user
+        pic.save()
+        return redirect(self.success_url)
+
+
+def stream_file(request, pk) :
+    pic = get_object_or_404(Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = pic.content_type
+    response['Content-Length'] = len(pic.picture)
+    response.write(pic.picture)
+    return response
